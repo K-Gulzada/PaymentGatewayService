cffc97741f7b5cb92303f4eceaeec28c8d16d267
ITstep <itstep@itstep.loc>
2021-12-15 21:51:28 +0600

    init

--- paymentgateway/views.py
+++ paymentgateway/views.py
@@ -1,4 +1,5 @@
-
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from paymentgateway.models import PaymentMethod
