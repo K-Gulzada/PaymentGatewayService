32022912b6831f93dfa60b1aba218fe5571c6abf
ITstep <itstep@itstep.loc>
2021-12-28 21:40:29 +0600

    transaction crud methods

--- paymentgateway/models/Transaction.py
+++ paymentgateway/models/Transaction.py
@@ -9,7 +9,7 @@ class Transaction(models.Model):
     tablename = "Transactions"
     orderInfo = models.CharField(max_length=255)
     sum = models.IntegerField
-    statusId = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE)
-    paymentMethodId = models.ForeignKey(PaymentStatus, on_delete=models.CASCADE)
+    statusId = models.ForeignKey(PaymentStatus, on_delete=models.CASCADE)
+    paymentMethodId = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE)
     BankId = models.ForeignKey(Bank, on_delete=models.CASCADE)
     date = models.DateField()
